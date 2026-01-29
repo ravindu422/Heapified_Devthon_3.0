20f1f33a4a3a063bf28725a0ae0acb999614d1b3
ravindu422 <thiranjayaravindu@gmail.com>
2026-01-30 01:44:00 +0530

    Created initial home page layout

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/common/Navbar'
+import Footer from '../components/common/Footer'
+
+
+const Home = () => {
+  return (
+    <div className='min-h-screen flex flex-col'>
+      <Navbar/>
+      <main className='grow'>
+
+      </main>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Home
