9e859d8703facfba81e722b0a3768e2b9909da85
Sashanka2001 <chamudisashanka01@gmail.com>
2026-01-30 21:28:11 +0530

    Add VolunteerDashboard route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Home from './pages/Home'
-import AlertManage from './pages/admin/AlertManage'
+import Home from "./pages/Home";
+import AlertManage from "./pages/admin/AlertManage";
+import VolunteerDashboard from "./components/dashboard/VolunteerDashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/alert-manage' element={<AlertManage/>}/>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/volunteer-dashboard"
+          element={<VolunteerDashboard />}
+        />
+        <Route path="/alert-manage" element={<AlertManage />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
