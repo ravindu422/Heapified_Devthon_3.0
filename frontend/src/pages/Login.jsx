468d5deefde47276bbc86e9250f5e5f2aca24e4f
sadumina <it23394124@my.sliit.lk>
2026-02-02 09:23:45 +0530

    add login page statment

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import AuthCard from "../components/AuthCard";
 import { login } from "../services/authService";
 import "../styles/auth.css";
@@ -13,9 +14,26 @@ export default function Login() {
 
   return (
     <AuthCard title="Welcome Back" subtitle="Please Enter Your Details">
-      <input placeholder="Email" onChange={e => setEmail(e.target.value)} />
-      <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+      
+      <input
+        placeholder="Email"
+        onChange={e => setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={e => setPassword(e.target.value)}
+      />
+
       <button onClick={handleLogin}>Login</button>
+
+      {/* 👇 Register Link */}
+      <p className="auth-link">
+        Don’t have an account?{" "}
+        <Link to="/register">Register here</Link>
+      </p>
+
     </AuthCard>
   );
 }
