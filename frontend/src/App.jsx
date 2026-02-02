2053b187681d217603846400060a96983a30ec32
ravindu422 <thiranjayaravindu@gmail.com>
2026-02-03 00:16:57 +0530

    Change of route: rename the AlertManage import path to new PublishAlert file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
 import Home from './pages/Home'
-import AlertManage from './pages/admin/AlertManage'
+import PublishAlert from './pages/admin/PublishAlert'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />}/>
-        <Route path='/alert-manage' element={<AlertManage/>}/>
+        <Route path='/alert-manage' element={<PublishAlert/>}/>
       </Routes>
     </BrowserRouter>
   )
