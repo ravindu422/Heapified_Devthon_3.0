f2159ef73649e8fadaf6c131e24ca5e00765df7a
sadumina <it23394124@my.sliit.lk>
2026-02-04 10:38:08 +0530

    update new files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Home from './pages/Home'
-import AlertManage from './pages/admin/AlertManage'
-import Login from './pages/Login'
-import Register from './pages/Register'
+import Home from "./pages/Home";
+import AlertManage from "./pages/admin/AlertManage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Unauthorized from "./pages/Unauthorized";
+
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -11,16 +14,26 @@ function App() {
       <Routes>
 
         {/* Public Routes */}
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
 
-        {/* Admin Routes */}
-        <Route path='/alert-manage' element={<AlertManage />} />
+        {/* Admin Routes (RBAC Protected) */}
+        <Route
+          path="/alert-manage"
+          element={
+            <ProtectedRoute roles={["ADMIN"]}>
+              <AlertManage />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Unauthorized */}
+        <Route path="/unauthorized" element={<Unauthorized />} />
 
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
