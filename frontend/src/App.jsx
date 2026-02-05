4158395d3716ce27de65f0efca96110cffb32eba
ravindu422 <thiranjayaravindu@gmail.com>
2026-02-05 16:52:37 +0530

    Added new browser router route path for AlertManage page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,15 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import PublishAlert from './pages/admin/PublishAlert'
+import AlertManage from './pages/admin/AlertManage'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />}/>
-        <Route path='/alert-manage' element={<PublishAlert/>}/>
+        <Route path='/publish-alert' element={<PublishAlert/>}/>
+        <Route path='/manage-alert' element={<AlertManage/>}/>
       </Routes>
     </BrowserRouter>
   )
