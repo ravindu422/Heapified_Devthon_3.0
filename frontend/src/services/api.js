a6d9176136a36a94463a46beb4bb6dc316ce35ba
ravindu422 <thiranjayaravindu@gmail.com>
2026-02-10 23:56:45 +0530

    Bug fix

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-const LOCAL_BACKEND = 'http://localhost:5080/api';
 
 const api = axios.create({
-    baseURL: import.meta.VITE_API_URL || LOCAL_BACKEND,
+    baseURL: import.meta.env.VITE_API_URL,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
